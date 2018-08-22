31b7d396e94960acaad2afd47a71de497cc98303
Anthony Curtis <tonydc1997@gmail.com>
2018-08-22 09:24:04 -0700

    Copy-paste code for other page components

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import logo from '../logo.svg';
+
+const Page2 = ({ onRouteChange }) => 
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <button onClick={() => onRouteChange('Page1')}>Page1</button>
+    <button onClick={() => onRouteChange('Page3')}>Page3</button>
+  </div>
+
+export default Page2;import React from 'react';
