9bcf1049799baba0e99862c106ce921f5c86aab3
Anthony Curtis <tonydc1997@gmail.com>
2018-08-22 09:20:15 -0700

    Destructure prop

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 
-const Page1 = () => 
+const Page1 = ({ onRouteChange }) => 
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
