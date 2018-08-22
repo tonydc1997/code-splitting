02cba3dbc00e08d24f0c8925e92029b32edf0e0d
Anthony Curtis <tonydc1997@gmail.com>
2018-08-22 09:12:40 -0700

    Build up Page1 component

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import logo from '../logo.svg';
+
+const Page1 = () => 
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <button onClick={}></button>
+  </div>
+
+export default Page1;
