024088f8cf0df4c40a8944070bdde756b4a500ea
Anthony Curtis <tonydc1997@gmail.com>
2018-08-22 09:26:29 -0700

    Import page components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 class App extends Component {
   constructor() {
