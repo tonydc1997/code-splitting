b27fd35b0b09ca1441797260f5301a94b8cec67a
Anthony Curtis <tonydc1997@gmail.com>
2018-08-22 09:29:13 -0700

    Remove redundant code

--- src/App.js
+++ src/App.js
@@ -18,17 +18,10 @@ onRouteChange = (route) => {
 }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+
         <Page1 onRouteChange={this.onRouteChange}/>
         <Page2 onRouteChange={this.onRouteChange}/>
         <Page3 onRouteChange={this.onRouteChange}/>
-      </div>
-    );
   }
 }
 
