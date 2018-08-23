351564d820e980b7d082b4f95e8c788fd155b3b6
Anthony Curtis <tonydc1997@gmail.com>
2018-08-23 09:10:38 -0700

    Import AsyncComponent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Page1 from './components/Page1';
 // import Page2 from './components/Page2';
 // import Page3 from './components/Page3';
+import AsyncComponent from './components/AsyncComponent';
 
 class App extends Component {
   constructor() {
