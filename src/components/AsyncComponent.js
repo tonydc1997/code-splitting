725e2ae54db0de4e767361dac5847c94c33428f2
Anthony Curtis <tonydc1997@gmail.com>
2018-08-23 09:06:46 -0700

    Build out render function

--- src/components/AsyncComponent.js
+++ src/components/AsyncComponent.js
@@ -8,11 +8,17 @@ export default function asyncComponent(importComponent) => {
         component: null
       }
     }
+
     async componentDidMount() {
       const { default: component } = await importComponent();
       this.setState({
         component: component
       })
+    }
+
+    render() {
+      const Component = this.state.component;
+      return Component ? <Component {...this.props} /> : null;
     }
   }
 }
