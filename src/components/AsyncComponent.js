1859201eef9eab12139c09b343ab2193ee1cf2c0
Anthony Curtis <tonydc1997@gmail.com>
2018-08-23 09:09:20 -0700

    Fix minor syntax error

--- src/components/AsyncComponent.js
+++ src/components/AsyncComponent.js
@@ -1,6 +1,6 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
-export default function asyncComponent(importComponent) => {
+export default function asyncComponent(importComponent) {
   class AsyncComponent extends Component {
     constructor(props) {
       super(props);
